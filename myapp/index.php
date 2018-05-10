7c58a1af1b9f07e6045a29a7277afca2bc1d0e10
Artem Daniliants <artem.daniliants@lumolink.com>
2018-05-10 11:36:28 +0300

    Got basic structure working

---
+++ myapp/index.php
@@ -0,0 +1,58 @@
+<?php
+require 'vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+$f3 = \Base::instance();
+
+$db=new DB\SQL(
+'mysql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT').';dbname='.getenv('DB_NAME'),
+getenv('DB_USER'),
+getenv('DB_PASS')
+);
+global $db;
+
+$f3->route('GET /',
+    function() {
+        echo 'Hello Docker webinar attendees :)';
+    }
+);
+
+$f3->route('GET /create_db',
+    function() {
+        $sql='CREATE TABLE People (
+	    LastName varchar(255),
+	    FirstName varchar(255)
+		);';
+		$GLOBALS['db']->exec($sql);
+		echo $GLOBALS['db']->log();
+    }
+);
+
+
+$f3->route('GET /insert_db',
+    function() {
+        $sql='INSERT INTO People VALUES ("Wick", "John")';
+		$GLOBALS['db']->exec($sql);
+		echo $GLOBALS['db']->log();
+    }
+);
+
+$f3->route('GET /read_db',
+    function() {
+        $sql='SELECT * from People';
+		var_dump($GLOBALS['db']->exec($sql));
+		echo $GLOBALS['db']->log();
+    }
+);
+
+$f3->route('GET /delete_db',
+    function() {
+        $sql='DELETE from People';
+		$GLOBALS['db']->exec($sql);
+		echo $GLOBALS['db']->log();
+    }
+);
+
+
+$f3->run();
